c0f08baa61972b03bb1875b3a176297595bf999e
Mau12HA <mau12herrera@gmail.com>
2025-05-12 20:36:19 -0600

    Agregando dos pruebas más a cada funcion

--- Semana_16/test_funciones_semana_6.py
+++ Semana_16/test_funciones_semana_6.py
@@ -3,7 +3,9 @@ from Semana_6.Funciones_Ejer_4 import string_back_way
 from Semana_6.Funciones_Ejer_5 import counter_lower_upper
 from Semana_6.Funciones_Ejer_6 import order_words_alpha
 from Semana_6.Funciones_Ejer_7 import list_prime_numbers
+import pytest
 
+# SUMA DE LISTA -------------------------------------------------
 
 def test_get_sum_list():
     # Arrange
@@ -14,6 +16,24 @@ def test_get_sum_list():
     # Assert
     assert result == expected_result
 
+def test_get_sum_list_exception_with_string():
+    # Arrange
+    test_list = ["a"]
+    # Act
+    with pytest.raises(TypeError):
+        get_sum_list(test_list)
+
+
+def test_get_sum_list_empty():
+    # Arrange
+    test_list = []
+    # Act
+    expected_result = 0
+    result = get_sum_list(test_list)
+    # Assert
+    assert result == expected_result
+
+# STRING BACK WAY -------------------------------------------------
 
 def test_string_back_way():
     # Arrange
@@ -25,6 +45,26 @@ def test_string_back_way():
     assert result == expected_result
 
 
+def test_string_back_way_empty():
+    # Arrange
+    test_string = ""
+    # Act
+    expected_result = ""
+    result = string_back_way(test_string)
+    # Assert
+    assert result == expected_result
+
+def test_string_back_way_with_numbers():
+    # Arrange
+    test_string = [1,2,3,4,5]
+    # Act
+    with pytest.raises(TypeError):
+        string_back_way(test_string)
+    
+
+
+# COUNTER LOWER UPPER -------------------------------------------------
+
 def test_counter_lower_upper():
     # Arrange
     test_string = "I love Nación Sushi"
@@ -34,6 +74,26 @@ def test_counter_lower_upper():
     # Assert
     assert result == expected_result
 
+def test_counter_lower_upper_empty():
+    # Arrange
+    test_string = ""
+    # Act
+    expected_result = {'Mayusculas': 0, 'minusculas': 0}
+    result = counter_lower_upper(test_string)
+    # Assert
+    assert result == expected_result
+
+def test_counter_lower_upper_with_numbers():
+    # Arrange
+    test_string = "12345"
+    # Act
+    expected_result = {'Mayusculas': 0, 'minusculas': 0}
+    result = counter_lower_upper(test_string)
+    # Assert
+    assert result == expected_result
+
+
+# ORDER WORDS ALPHA -------------------------------------------------
 
 def test_order_words_alpha():
     # Arrange
@@ -45,6 +105,29 @@ def test_order_words_alpha():
     assert result == expected_result
 
 
+def test_order_words_alpha_empty():
+    # Arrange
+    test_string = ""
+    # Act
+    expected_result = ""
+    result = order_words_alpha(test_string)
+    # Assert
+    assert result == expected_result
+
+
+def test_order_words_alpha_with_numbers():
+    # Arrange
+    test_string = "12345-67890"
+    # Act
+    expected_result = "12345-67890"
+    result = order_words_alpha(test_string)
+    # Assert
+    assert result == expected_result
+
+
+
+# PRIME NUMBERS -------------------------------------------------
+
 def test_list_prime_numbers():
     # Arrange
     test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -53,5 +136,21 @@ def test_list_prime_numbers():
     result = list_prime_numbers(test_list, []) 
     # Assert
     assert result == expected_result
+
+def test_list_prime_numbers_empty():
+    # Arrange
+    test_list = []
+    # Act
+    expected_result = []
+    result = list_prime_numbers(test_list, [])
+    # Assert
+    assert result == expected_result
+
+def test_list_prime_numbers_with_strings():
+    # Arrange
+    test_list = ["a", "b", "c"]
+    # Act
+    with pytest.raises(TypeError):
+        list_prime_numbers(test_list, [])
 
 
